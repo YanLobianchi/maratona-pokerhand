74476637d30ca46345ffebb5bbcee15add0e2c6e
Gilson Alves <gilson.silva@zgsolucoes.com.br>
2020-02-07 10:49:50 -0300

    Adicionando estrutura para requisições http

---
+++ src/main/groovy/br/com/zgsolucoes/maratonapokerhand/service/HttpService.groovy
@@ -0,0 +1,55 @@
+package br.com.zgsolucoes.maratonapokerhand.service
+
+import com.google.gson.Gson
+import org.apache.http.HttpHeaders
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.client.methods.HttpUriRequest
+import org.apache.http.client.methods.RequestBuilder
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.util.EntityUtils
+
+class HttpService {
+	List<String> sendRequest(String url, String method, Map<String, Object> body = null) {
+		RequestConfig requestConfig = RequestConfig.custom()
+				.setConnectTimeout(2000)
+				.setSocketTimeout(3000)
+				.build()
+		HttpUriRequest request = null
+
+		if (body) {
+			StringEntity entity = new StringEntity(new Gson().toJson(body), "UTF-8")
+			request = RequestBuilder.create(method)
+					.setConfig(requestConfig)
+					.setUri(url)
+					.setHeader(HttpHeaders.CONTENT_TYPE, "text/html")
+					.setEntity(entity)
+					.build()
+		} else {
+			request = RequestBuilder.create(method)
+					.setConfig(requestConfig)
+					.setUri(url)
+					.setHeader(HttpHeaders.CONTENT_TYPE, "text/html")
+					.build()
+		}
+
+		String req = "REQUEST:" + "\n" + request.getRequestLine() + "\n" + "Headers: " +
+				request.getAllHeaders() + "\n" + EntityUtils.toString() + "\n"
+
+		//todo criar um map ou objeto para armazenar a resposta da requisicao
+
+		HttpClientBuilder.create().build().withCloseable { httpClient ->
+
+			httpClient.execute(request).withCloseable { response ->
+
+				String res = "RESPONSE:" + "\n" + response.getStatusLine() + "\n" + "Headers: " +
+						response.getAllHeaders() + "\n" +
+						(response.getEntity() != null ? EntityUtils.toString(response.getEntity()) : "") + "\n"
+
+				System.out.println(req + "\n" + res)
+
+				return Arrays.asList(req, res)
+			}
+		}
+	}
+}
---
+++ src/test/groovy/HttpServiceSpec.groovy
@@ -0,0 +1,18 @@
+import br.com.zgsolucoes.maratonapokerhand.service.HttpService
+import spock.lang.Specification
+
+class HttpServiceSpec extends Specification {
+
+	void 'testa resultado requisicao'() {
+		setup:
+		HttpService httpAcces = new HttpService()
+		String url = 'http://172.22.1.41:8080/poker-game/arquivo/index'
+
+		when:
+		List response = httpAcces.sendRequest(url, 'GET')
+
+		then:
+		response
+	}
+
+}

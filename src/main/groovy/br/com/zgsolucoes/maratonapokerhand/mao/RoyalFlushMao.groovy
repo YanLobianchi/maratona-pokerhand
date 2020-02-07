e81d3f4af17e1f840d674ef0e987f064a1984acf
Yan Lobianchi <yan.cruz@zgsolucoes.com.br>
2020-02-07 18:00:56 -0300

    feature 'mao': - adicionando rodada no ResultadoRodada

--- src/main/groovy/br/com/zgsolucoes/maratonapokerhand/mao/RoyalFlushMao.groovy
+++ src/main/groovy/br/com/zgsolucoes/maratonapokerhand/mao/RoyalFlushMao.groovy
@@ -6,7 +6,8 @@ class RoyalFlushMao extends Mao {
 
 	@Override
 	ResultadoMao reconhecerMao(final MaoHelper maoHelper) {
-		return maoHelper.cartasEmSequencia == maoHelper.cartasMesmoNaipe && maoHelper.cartasEmSequencia.last().valorCarta == ValorCarta.AS ?
+		return maoHelper.cartasEmSequencia && maoHelper.cartasMesmoNaipe &&
+					   maoHelper.cartasEmSequencia == maoHelper.cartasMesmoNaipe && maoHelper.cartasEmSequencia.last().valorCarta == ValorCarta.AS ?
 			   new ResultadoMao(Categoria.ROYAL_FLUSH, maoHelper.cartasEmSequencia) : null
 	}
 }
